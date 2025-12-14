848f6d76234769e0a4c64cd90dffe38f910ecd14
Connor Beaven <Cmbeaven1@gmail.com>
2025-12-14 15:55:33 -0500

    Add problem 5 input parsing

--- 05.cpp
+++ 05.cpp
@@ -4,7 +4,23 @@
  * Find rotten ingredients in the elves database
  */
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 int main(int argv, char** argc){
     
+    std::string inputLine;
+    getline(std::cin,inputLine);
+    do{
+        std::cout << inputLine << '\n';
+        getline(std::cin,inputLine);
+    }while(inputLine != "");
+
+    while(!std::cin.eof()){
+        getline(std::cin,inputLine);
+        std::cout << inputLine << '\n';
+    }
+
     return 0;
 }
