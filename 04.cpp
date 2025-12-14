307e3b45362c0da7925de83500d2a753d4a348ee
Connor Beaven <Cmbeaven1@gmail.com>
2025-12-14 13:22:32 -0500

    Add matrix class

--- 04.cpp
+++ 04.cpp
@@ -4,7 +4,42 @@
  * Helps the elves maximize their efficiency in the printing dept.
  */
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string_view>
+
+class charMatrix{
+    public:
+        void addRow(std::string_view row);
+        char getLocation(int x, int y);
+    private:
+        int rowLength = 0;
+        std::vector<std::string> matrix;
+};
+
 int main(int argv, char** argc){
-    
+
     return 0;
+}
+
+void charMatrix::addRow(std::string_view row){
+    if(rowLength == 0)
+        rowLength = row.length();
+    else if(rowLength != row.length())
+        return;
+    matrix.push_back(row.data());
+}
+char charMatrix::getLocation(int x, int y){
+    // check bounds
+    if(x < 0)
+        return 0;
+    if(y < 0)
+        return 0;
+    if(y > rowLength)
+        return 0;
+    if(x > matrix.size())
+        return 0;
+    
+    return matrix[x][y];
 }
