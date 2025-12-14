de649ca91760a3fac221309a40030f7c889e6cb7
Connor Beaven <Cmbeaven1@gmail.com>
2025-12-14 16:01:44 -0500

    store input data

--- 05.cpp
+++ 05.cpp
@@ -7,18 +7,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <utility>
+
+typedef std::pair<long,long> range;
 
 int main(int argv, char** argc){
     
     std::string inputLine;
+    std::vector<range> freshRanges;
+    std::vector<long> ingredients;
     getline(std::cin,inputLine);
     do{
-        std::cout << inputLine << '\n';
+        int splitLocation = inputLine.find('-');
+        range temp;
+        temp.first = std::stol(inputLine.substr(0,splitLocation));
+        temp.second = std::stol(inputLine.substr(splitLocation+1,inputLine.length()));
+        std::cout << temp.first << "-" << temp.second << "\n";
+        freshRanges.push_back(temp);
         getline(std::cin,inputLine);
     }while(inputLine != "");
 
     while(!std::cin.eof()){
         getline(std::cin,inputLine);
+        ingredients.push_back(std::stol(inputLine));
         std::cout << inputLine << '\n';
     }
 
